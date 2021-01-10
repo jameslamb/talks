bb440edef4a6f2834cb4dbe0b1b3d0ecb35a4b98
James Lamb <jaylamb20@gmail.com>
2021-01-10 12:07:45 -0600

    [ci] add CI job to lint Python  code (#20)
    * [ci] add CI job to lint Python  code
    
    * add an all-successful job
    
    * blegh

--- cloud-intro/lambdas/predict.py
+++ cloud-intro/lambdas/predict.py
@@ -4,9 +4,9 @@ import os
 import numpy as np
 import pickle
 import pandas as pd
-from ticket_closure_lib.transformers import OrdinalConverter
-from ticket_closure_lib.transformers import DateColTransformer
-from ticket_closure_lib.transformers import FeatureRemover
+from ticket_closure_lib.transformers import OrdinalConverter  # noqa
+from ticket_closure_lib.transformers import DateColTransformer  # noqa
+from ticket_closure_lib.transformers import FeatureRemover  # noqa
 
 
 class TicketPredictor:
@@ -31,7 +31,7 @@ class TicketPredictor:
             "assignment_group",
             "u_priority_confirmation",
             "notify",
-            "problem_id"
+            "problem_id",
         ]
 
     def predict_json(self, in_data):
@@ -42,10 +42,7 @@ class TicketPredictor:
         # make sure column order is right
         in_df = in_df[self.columns]
         for date_col in ["opened_at", "sys_created_at", "sys_updated_at"]:
-            in_df[date_col] = pd.to_datetime(
-                in_df[date_col],
-                format="%d/%m/%Y %H:%M"
-            )
+            in_df[date_col] = pd.to_datetime(in_df[date_col], format="%d/%m/%Y %H:%M")
         preds = self.mod.predict(in_df)
         return list(np.round(preds, 2))
 
@@ -54,24 +51,15 @@ def lambda_handler(event, context):
     model_file = "/tmp/model.pkl"
     if not os.path.isfile(model_file):
         print("file not found, getting it from S3")
-        S3 = boto3.resource('s3')
+        S3 = boto3.resource("s3")
         S3.meta.client.download_file(
-            os.environ["TRAINING_ARTIFACT_BUCKET"],
-            os.environ["MODEL_FILE"],
-            model_file
+            os.environ["TRAINING_ARTIFACT_BUCKET"], os.environ["MODEL_FILE"], model_file
         )
     predictor = TicketPredictor(model_file=model_file)
-    return {
-        "predictions": predictor.predict_json(event["data"])
-    }
+    return {"predictions": predictor.predict_json(event["data"])}
 
 
 if __name__ == "__main__":
     with open("test-preds.json", "r") as f:
-        pred_json = {
-            "data": json.loads(f.read())
-        }
-    print(lambda_handler(
-        event=pred_json,
-        context=False
-    ))
+        pred_json = {"data": json.loads(f.read())}
+    print(lambda_handler(event=pred_json, context=False))
--- cloud-intro/ticket_closure_lib/transformers.py
+++ cloud-intro/ticket_closure_lib/transformers.py
@@ -13,6 +13,7 @@ class OrdinalConverter(BaseEstimator, TransformerMixin):
     allowing OrdinalEncoder to make itt up based
     on the order of occurrence in the data.
     """
+
     def __init__(self, feature_map: Dict[str, Dict[str, int]]):
         # Map of {feature_name: levels}, where
         #    * feature_name: a column name in the training data
@@ -20,45 +21,49 @@ class OrdinalConverter(BaseEstimator, TransformerMixin):
         #              the column and values are the numeric value
         #              to replace them with
         self.feature_map = feature_map
-        
-    def fit(self, X, y = None):
+
+    def fit(self, X, y=None):
         return self
-        
+
     def transform(self, X, y=None):
         assert isinstance(X, pd.DataFrame)
         X = X.copy()
         for feat_col, levels in self.feature_map.items():
-            X[feat_col] = X[feat_col].map(levels).astype('int')
+            X[feat_col] = X[feat_col].map(levels).astype("int")
         return X
+
 
 class DateColTransformer(BaseEstimator, TransformerMixin):
     """
     Custom feature-creation class
     """
+
     def __init__(self):
         pass
 
-    def fit(self, X, y = None):
-        return self 
-    
-    def transform(self, X, y = None):
-        #assert isinstance(X, pd.DataFrame)
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        # assert isinstance(X, pd.DataFrame)
         X = X.copy()
-        creation_col = 'opened_at'
-        X['time_til_update'] = (X['sys_updated_at'] - X[creation_col]) / np.timedelta64(1, 's')
-        X['open_hour_of_day'] = X[creation_col].dt.hour
+        creation_col = "opened_at"
+        X["time_til_update"] = (X["sys_updated_at"] - X[creation_col]) / np.timedelta64(1, "s")
+        X["open_hour_of_day"] = X[creation_col].dt.hour
         return X
+
 
 class FeatureRemover(BaseEstimator, TransformerMixin):
     """
     Drop columns from the data. This is useful for
     discarding intermediate features or
-    raw columns that you don't want to train on. 
+    raw columns that you don't want to train on.
     """
+
     def __init__(self, cols_to_drop):
         self.cols_to_drop = cols_to_drop
 
-    def fit(self, X, y = None):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
